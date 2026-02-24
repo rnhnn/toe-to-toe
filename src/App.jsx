db709dc0b5c677e858d04c6c99e63bc1b8982dd7
Rolando <rolandohernandezleal@gmail.com>
2026-02-24 10:10:53 -0300

    fix paths and clean up project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="app">
+      <h1>Toe to Toe</h1>
+      <p>Beatles-flavored Tic-Tac-Toe (coming soon)</p>
+    </main>
+  );
+}
